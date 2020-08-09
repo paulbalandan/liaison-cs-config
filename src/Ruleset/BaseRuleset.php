bcb43f8fa8d31e2e4bd3dfaac4d1f0014dda9cc5
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2020-08-10 00:08:22 +0800

    Add abstract BaseRuleset

---
+++ src/Ruleset/BaseRuleset.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * This file is part of Liaison CS Config Factory.
+ *
+ * (c) John Paul E. Balandan, CPA <paulbalandan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Liaison\CS\Config\Ruleset;
+
+abstract class BaseRuleset implements RulesetInterface
+{
+    /**
+     * Name of the ruleset.
+     *
+     * @var string
+     */
+    protected $name = '';
+
+    /**
+     * Rules for the ruleset.
+     *
+     * @var array
+     */
+    protected $rules = [];
+
+    /**
+     * Minimum PHP version.
+     *
+     * @var int
+     */
+    protected $requiredPHPVersion = 0;
+
+    /**
+     * Is this ruleset risky?
+     *
+     * @var bool
+     */
+    protected $isRisky = false;
+
+    /**
+     * Constructor.
+     *
+     * @param null|string $header header to include
+     */
+    final public function __construct(?string $header = null)
+    {
+        if (null !== $header) {
+            $this->rules['header_comment'] = [
+                'header'       => trim($header),
+                'comment_type' => 'PHPDoc',
+            ];
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    final public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    final public function getRules(): array
+    {
+        return $this->rules;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    final public function getRequiredPHPVersion(): int
+    {
+        return $this->requiredPHPVersion;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    final public function isRiskyRuleset(): bool
+    {
+        return $this->isRisky;
+    }
+}
