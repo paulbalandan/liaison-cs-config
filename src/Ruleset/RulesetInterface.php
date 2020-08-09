0631f4bd0e99952bd05a6cd05aa25fca7b15ca8f
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2020-08-10 00:07:40 +0800

    Add RulesetInterface

---
+++ src/Ruleset/RulesetInterface.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This file is part of Liaison CS Config Factory.
+ *
+ * (c) John Paul E. Balandan, CPA <paulbalandan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Liaison\CS\Config\Ruleset;
+
+interface RulesetInterface
+{
+    /**
+     * Name of this ruleset.
+     *
+     * @return string
+     */
+    public function getName(): string;
+
+    /**
+     * Defined rules for this ruleset.
+     *
+     * @return array
+     */
+    public function getRules(): array;
+
+    /**
+     * Returns the minimum `PHP_VERSION_ID`
+     * that is required by this ruleset.
+     *
+     * @return int
+     */
+    public function getRequiredPHPVersion(): int;
+
+    /**
+     * Does this ruleset have risky rules?
+     *
+     * @return bool
+     */
+    public function isRiskyRuleset(): bool;
+}
